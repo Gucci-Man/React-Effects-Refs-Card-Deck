86f50408627529fa13ebe7f2bd80ffc1b457164e
mrtomno <adel.ngo@yahoo.com>
2024-02-02 23:03:20 -0600

    create shuffle function

--- src/Deck.js
+++ src/Deck.js
@@ -18,12 +18,56 @@ function Deck() {
         fetchData();
     }, []);
 
+    /** Draw card: change the state & effect will kick in. */
+    async function draw() {
+        try {
+            const drawRes = await axios.get(`${API_BASE_URL}/${deck.deck_id}/draw/`);
+
+            if (drawRes.data.remaining === 0) throw new Error("Deck empty!");
+
+            const card = drawRes.data.cards[0];
+
+            setDrawn(d => [
+                ...d,
+                {
+                    id: card.code,
+                    name: card.suit + " " + card.value,
+                    image: card.image,
+                },
+            ]);
+        } catch (err){
+            alert(err);
+        }
+    }
+
+    /** Shuffle: change the state & effect will kick in */
+    async function startShuffling() {
+        setIsShuffling(true);
+        try {
+            await axios.get(`${API_BASE_URL}/${deck.deck_id}/shuffle/`);
+            setDrawn([]);
+        } catch (err) {
+            alert(err);
+        } finally {
+            setIsShuffling(false);
+        }
+    }
+
+    /** Return draw button (disabled if shuffling) */
+    function renderDrawBtnIfOk() {
+        if (!deck) return null;
     
+
     return (
-        <div>
-            <h1>Hello there</h1>
-        </div>
-    )
+        <button
+            className="Deck-gimme"
+            onCLick={draw}
+            disabled={isShuffling}>
+            DRAW
+        </button>
+        );
+    }
+
 };
 
 export default Deck;
