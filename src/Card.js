814f7f5020e9bb93934e6df8a5247792933c824b
mrtomno <adel.ngo@yahoo.com>
2024-02-02 21:59:57 -0600

    create card

--- src/Card.js
+++ src/Card.js
@@ -3,4 +3,20 @@ import "./Card.css"
 
 function Card({name, image}) {
     // get info for cards once, will not be updated for the same card
+
+    const [{ angle, xPos, yPos }] = useState({
+        angle: Math.random() * 90 - 45,
+        xPos: Math.random() * 40 - 20,
+        yPos: Math.random() * 40 - 20,
+    });
+
+    const transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+
+    return <img 
+        className="Card"
+        alt={name}
+        src={image}
+        style={( transform )} />;
 }
+
+export default Card;
--- src/Deck.js
+++ src/Deck.js
@@ -1,1 +1,11 @@
 import React , {useEffect, useState } from "react";
+
+function Deck() {
+    return (
+        <div>
+            <h1>Hello there</h1>
+        </div>
+    )
+};
+
+export default Deck;
